196e9ca0a1eeff0315dda70053babecc0f31e467
Librame <pengxin@live.cn>
2017-06-11 14:09:50 +0800

    fixed-201706111

--- examples/LibrameCore.Website/Controllers/UserController.cs
+++ examples/LibrameCore.Website/Controllers/UserController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http.Authentication;
 
 namespace LibrameCore.Website.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Login()
+        public async Task<IActionResult> Login()
         {
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, "userid"));
+            await HttpContext.Authentication.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(identity), new AuthenticationProperties() { IsPersistent = true });
+
             return View();
         }
 
---
+++ src/LibrameCore/Utilities/HttpRequestUtility.cs
@@ -0,0 +1,34 @@
+﻿#region License
+
+/* **************************************************************************************
+ * Copyright (c) Librame Pang All rights reserved.
+ * 
+ * http://librame.net
+ * 
+ * You must not remove this notice, or any other, from this software.
+ * **************************************************************************************/
+
+#endregion
+
+using Microsoft.AspNetCore.Http;
+
+namespace LibrameStandard.Utilities
+{
+    /// <summary>
+    /// <see cref="HttpRequest"/> 实用工具。
+    /// </summary>
+    public static class HttpRequestUtility
+    {
+
+        /// <summary>
+        /// 转换为 URI 格式。
+        /// </summary>
+        /// <param name="request">给定的 HTTP 请求。</param>
+        /// <returns>返回字符串。</returns>
+        public static string AsUriString(this HttpRequest request)
+        {
+            return (request.Scheme + "://" + request.Host + request.PathBase + request.Path + request.QueryString);
+        }
+
+    }
+}
